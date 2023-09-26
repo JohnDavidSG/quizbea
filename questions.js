131c10cda5e2bba58aa00732c9c002e39ae210c2
JohnDavidSG <141514058+JohnDavidSG@users.noreply.github.com>
2023-09-26 02:07:40 -0300

    Arquivos do site

---
+++ questions.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    question: "Dentre esses qual eu gostaria mais?",
+    answers: [
+      { option: "Bacon", correct: false },
+      { option: "Lasanha", correct: false },
+      { option: "Orégano", correct: true },
+    ],
+  },
+  {
+    question: "Qual país é meu preferido",
+    answers: [
+      { option: "Alemanha", correct: false },
+      { option: "Suiça", correct: true },
+      { option: "Espanha", correct: false },
+    ],
+  },
+  {
+    question: "Qual arte marcial eu já pratiquei?",
+    answers: [
+      { option: "Judo", correct: true },
+      { option: "Karate", correct: false },
+      { option: "Jiu Jitsu", correct: false },
+    ],
+  },
+  {
+    question: "Em qual destes esportes ja fui premiado",
+    answers: [
+      { option: "Judo", correct: false },
+      { option: "Atletismo", correct: true },
+      { option: "Futebol", correct: false },
+    ],
+  },
+  {
+    question: "Se eu pudesse escolher, qual eu escolheria?",
+    answers: [
+      { option: "Orégano", correct: false },
+      { option: "Avião ", correct: true },
+      { option: "Eletricidade", correct: false },
+    ],
+  },
+  {
+    question: "Qual o meu maior sonho?",
+    answers: [
+      { option: "Ser piloto", correct: false },
+      { option: "Ser médico", correct: false },
+      { option: "Ser bem-sucedido", correct: true },
+    ],
+  },
+  {
+    question: "Qual curso é meu preferido?",
+    answers: [
+      { option: "Administração", correct: true },
+      { option: "Eletrônica", correct: false },
+      { option: "Educação financeira", correct: false },
+    ],
+  },
+  {
+    question: "Qual meu nome completo?",
+    answers: [
+      { option: "John David Silva Gonçalves", correct: true },
+      { option: "John David Gonçalves Da Silva", correct: false },
+      { option: "John David Silva Gonçalves De Oliveira", correct: false },
+    ],
+  },
+  {
+    question: "Eu como bastante?",
+    answers: [
+      { option: "Sim muito!!", correct: false },
+      { option: "VC NÃO PODE COMER 3 PRATOS", correct: true },
+      { option: "Demaisss!!!!", correct: false },
+    ],
+  },
+  {
+    question: "Quem vai me ensinar o cubo magico :D ???",
+    answers: [
+      { option: "↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓", correct: false },
+      { option: "B.A.R.M", correct: true },
+      { option: "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑", correct: false },
+    ],
+  },
+];
